970a9a334fcb8d686c77ed19eb7434bd6bff0134
Fahad <205fahadmohammad@gmail.com>
2024-02-22 19:51:59 +0600

    [done] contact page completed

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,17 +1,94 @@
 import DynamicBanner from "@/components/ui/shared/DynamicBanner";
+import EmailForm from "@/components/ui/shared/EmailForm";
 import React from "react";
 import { FiPhoneCall } from "react-icons/fi";
+import { HiOutlineMailOpen } from "react-icons/hi";
+import { TbMap2 } from "react-icons/tb";
 
 const ContactPage = () => {
   return (
     <section>
       <DynamicBanner title="Contact Us" />
 
-      <div className="mt-[95px] container mx-auto">
-        <div>
-          <FiPhoneCall />
+      <div className="mt-[95px] container mx-auto grid grid-cols-2 gap-x-[50px]">
+        <div className="">
+          <h2 className="text-5xl font-bold mb-[55px]">
+            Feel free to contact with us for any query.
+          </h2>
+          <div className="flex gap-x-[15px] items-center pl-5 pt-[23px] pb-[23px] bg-[#F3F2F2] rounded-r-full w-[369px]">
+            <div className="p-[10px] bg-srPrimary w-fit rounded">
+              <FiPhoneCall className="text-[34px] text-white" />
+            </div>
+            <div>
+              <h4 className="text-xl font-bold">Phone Number</h4>
+              <p className="text-xs font-normal">Head Office. (210) 123 - 23</p>
+            </div>
+          </div>
+          <div className="flex gap-x-[15px] items-center pl-5 pt-[23px] pb-[23px] w-[369px] rounded-r-full mt-5 border border-[#BCBCBC]">
+            <div className="p-[10px] bg-srPrimary w-fit rounded">
+              <HiOutlineMailOpen className="text-[34px] text-white" />
+            </div>
+            <div>
+              <h4 className="text-xl font-bold">Mail Address</h4>
+              <p className="text-xs font-normal">swiftrealty@gmail.com</p>
+            </div>
+          </div>
+          <div className="flex gap-x-[15px] items-center pl-5 pt-[23px] pb-[23px] rounded-r-full w-[369px] mt-5 border border-[#BCBCBC]">
+            <div className="p-[10px] bg-srPrimary w-fit rounded">
+              <TbMap2 className="text-[34px] text-white" />
+            </div>
+            <div>
+              <h4 className="text-xl font-bold">Office Location</h4>
+              <p className="text-xs font-normal">
+                34 Mawa Road, Mawa Port, Dhaka
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className=" bg-[#F3F2F2] px-[30px] py-10">
+          <input
+            className="p-5 bg-transparent border w-full border-mutedText"
+            type="text"
+            name=""
+            id=""
+            placeholder="Full Name *"
+          />
+          <div className="flex gap-x-5 mt-5">
+            <input
+              className="p-5 bg-transparent border w-full border-mutedText"
+              type="email"
+              name=""
+              id=""
+              placeholder="Email Address *"
+            />
+            <input
+              className="p-5 bg-transparent border w-full border-mutedText"
+              type="number"
+              name=""
+              id=""
+              placeholder="Phone *"
+            />
+          </div>
+          <select
+            className="p-5 bg-transparent border w-full border-mutedText text-mutedText mt-5"
+            name="properties"
+            id="properties"
+          >
+            <option value="1">Select Properties</option>
+          </select>
+          <textarea
+            className="mt-5 p-5 text-mutedText w-full bg-transparent border border-mutedText"
+            name=""
+            id=""
+          >
+            Write Message
+          </textarea>
+          <button className="mt-[30px] bg-srPrimary px-[30px] py-[15px] rounded-sm">
+            Submit Request
+          </button>
         </div>
       </div>
+      <EmailForm />
     </section>
   );
 };
