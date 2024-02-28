921003dc8ba084ffdd94974fd866f4bef4e9245d
Fahad <205fahadmohammad@gmail.com>
2024-02-28 19:32:51 +0600

    [done] search and filter responsive

--- src/components/ui/homepage/SearchFilter.tsx
+++ src/components/ui/homepage/SearchFilter.tsx
@@ -6,13 +6,13 @@ import chart from "@/assets/images/chart-pie.png";
 
 const SearchFilter = () => {
   return (
-    <div className="lg:w-[1120px] h-[246px] relative mx-auto z-30 bg-[#FCFCFF] shadow-md rounded-xl">
-      <div className="lg:w-[1120px] h-[246px] left-0 top-0 absolute" />
-      <div className="left-[33px] top-[46px] absolute text-black lg:text-4xl font-semibold font-['Inter']">
+    <div className="lg:w-[1120px] lg:h-[246px] lg:relative mx-auto z-30 bg-[#FCFCFF] shadow-md rounded-xl mt-5 lg:mt-0">
+      <div className="lg:w-[1120px] lg:h-[246px] left-0 top-0 lg:absolute" />
+      <div className="lg:left-[33px] lg:top-[46px] lg:absolute text-center text-black lg:text-4xl font-semibold font-['Inter']">
         Search the price you looking for
       </div>
-      <div className="relative top-[129px] px-8 lg:flex gap-5 items-center">
-        <div className="relative max-w-xs mt-2 w-[280px]">
+      <div className="relative lg:top-[129px] lg:px-8 lg:flex gap-5 items-center">
+        <div className="relative max-w-xs mt-2 w-[280px] mx-auto">
           <button className="text-gray-400 absolute right-3 inset-y-0 my-auto active:text-gray-600">
             <Image src={location} width={24} height={24} alt="map-pin" />
           </button>
@@ -22,7 +22,7 @@ const SearchFilter = () => {
             className="w-full p-5 bg-[#0000000D] text-[#BCBCBC] text-2xl font-semibold font-[Inter] rounded-lg"
           />
         </div>
-        <div className="relative max-w-xs mt-2 w-[280px]">
+        <div className="relative max-w-xs mt-2 w-[280px] mx-auto">
           <button className="text-gray-400 absolute right-3 inset-y-0 my-auto active:text-gray-600">
             <Image src={home} width={24} height={24} alt="map-pin" />
           </button>
@@ -32,7 +32,7 @@ const SearchFilter = () => {
             className="w-full p-5 bg-[#0000000D] text-[#BCBCBC] text-2xl font-semibold font-[Inter] rounded-lg"
           />
         </div>
-        <div className="relative max-w-xs mt-2 w-[280px]">
+        <div className="relative max-w-xs mt-2 w-[280px] mx-auto">
           <button className="text-gray-400 absolute right-3 inset-y-0 my-auto active:text-gray-600">
             <Image src={chart} width={24} height={24} alt="map-pin" />
           </button>
@@ -42,7 +42,7 @@ const SearchFilter = () => {
             className="w-full p-5 bg-[#0000000D] text-[#BCBCBC] text-2xl font-semibold font-[Inter] rounded-lg"
           />
         </div>
-        <button className="bg-srPrimary text-white py-5 text-2xl h-[71px] font-bold font-[Inter] px-[38px] rounded-lg">
+        <button className="bg-srPrimary text-white lg:py-5 py-2 lg:text-2xl text-lg lg:h-[71px] font-bold font-[Inter] lg:px-[38px] px-[18px] rounded-lg my-5 lg:my-0 block mx-auto">
           Search
         </button>
       </div>
